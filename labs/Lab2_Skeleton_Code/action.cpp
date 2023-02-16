97ace6cdb468e8a3ebd5af4828fd9a1ee2a6e1ea
Carter Pollan <carter.pollan@gmail.com>
2023-02-16 15:23:46 -0700

    Updated action.cpp to post Lab 2 code

--- labs/Lab2_Skeleton_Code/action.cpp
+++ labs/Lab2_Skeleton_Code/action.cpp
@@ -3,81 +3,86 @@
 /**********************************************************************************************************
   Robot ACTION - implementing the decisions from planning to specific actions
  ********************************************************************/
-void RobotAction()
-{
-    // Here the results of planning are implented so the robot does something
+void RobotAction() {
+  // Here the results of planning are implented so the robot does something
 
-    // This turns the collision LED on and off
-    switch (ActionCollision)
-    {
+  // This turns the collision LED on and off
+  switch (ActionCollision) {
     case COLLISION_OFF:
-        doTurnLedOff(LED_3); // OR ELSE YOUR LEDS WON'T WORK!!!
-        break;
+      doTurnLedOff(LED_3); //OR ELSE YOUR LEDS WON'T WORK!!!
+      break;
     case COLLISION_ON:
-        doTurnLedOn(LED_3);
-        break;
-    }
+      doTurnLedOn(LED_3);
+      break;
+  }
 
-    // Prints the volatge of BUTTON_3
-    Serial.print(getPinVoltage(BUTTON_3));
-    Serial.print("\n");
+  //Prints the volatge of BUTTON_3
+  //  Serial.print(getPinVoltage(BUTTON_3));
+  //  Serial.print("\n");
 
-    // This drives the main motors on the robot
-    switch (ActionRobotDrive)
-    {
+  // This drives the main motors on the robot
+  switch (ActionRobotDrive) {
     case DRIVE_STOP:
-        /* Add code in milestone 2 to turn off both left and right motors (LEDs right now).
-          Use the doTurnLedOff() function */
-        /* DON'T FORGET TO USE YOUR LED VARIABLES AND NOT YOUR BUTTON VARIABLES FOR THIS!!! */
-        break;
+      //control the LEDs
+      doTurnLedOff(LED_2);
+      doTurnLedOff(LED_4);
+      break;
     case DRIVE_LEFT:
-        /* Add code in milestone 2 to turn off the right and on the left LEDs */
-        break;
+      //control the LEDs
+      doTurnLedOn(LED_2);
+      doTurnLedOff(LED_4);
+      break;
     case DRIVE_RIGHT:
-        /* Add code in milestone 2 */
-        break;
+      //control the LEDs
+      doTurnLedOff(LED_2);
+      doTurnLedOn(LED_4);
+      break;
     case DRIVE_STRAIGHT:
-        /* Add code in milestone 2 */
-        break;
-    }
+      doTurnLedOn(LED_2);
+      doTurnLedOn(LED_4);
+      break;
+  }
 
-    // This calls a function to move the servo
-    MoveServo();
+  // This calls a function to move the servo
+  MoveServo();
 }
+
 
 ////////////////////////////////////////////////////////////////////
 // Function that causes the servo to move up or down.
 ////////////////////////////////////////////////////////////////////
-void MoveServo()
-{
-    // Note that there needs to be some logic in the action of moving
-    // the servo so that it does not exceed its range
-    /* Add CurrentServoAngle in lab 6 */
-    switch (ActionServoMove)
-    {
+void MoveServo() {
+  // Note that there needs to be some logic in the action of moving
+  // the servo so that it does not exceed its range
+  /* Add CurrentServoAngle in lab 6 */
+  switch (ActionServoMove) {
     case SERVO_MOVE_STOP:
-        /* Add code in milestone 3 */
-        break;
+      doTurnLedOff(LED_5);
+      doTurnLedOff(LED_1);
+      break;
     case SERVO_MOVE_UP:
-        /* Add code in milestone 3 */
-        break;
+      doTurnLedOff(LED_5);
+      doTurnLedOn(LED_1);
+      break;
     case SERVO_MOVE_DOWN:
-        /* Add code in milestone 3 */
-        break;
-    }
+      doTurnLedOn(LED_5);
+      doTurnLedOff(LED_1);
+      break;
+  }
 }
 
-/**********************************************************************************************************
+
+
+
+/********************************************************************
   AUXILIARY functions that may be useful in performing diagnostics
  ********************************************************************/
 // Function to turn LED on
-void doTurnLedOn(int led_pin)
-{
-    digitalWrite(led_pin, HIGH);
+void doTurnLedOn(int led_pin) {
+  digitalWrite(led_pin, HIGH);
 }
 
 // Function to turn LED off
-void doTurnLedOff(int led_pin)
-{
-    digitalWrite(led_pin, LOW);
+void doTurnLedOff(int led_pin) {
+  digitalWrite(led_pin, LOW);
 }
