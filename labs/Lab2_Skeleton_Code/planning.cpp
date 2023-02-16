58190cae19e2bc7604c858dd54e52fc780ec6ac0
Carter Pollan <carter.pollan@gmail.com>
2023-02-16 15:26:41 -0700

    updated planning to post lab 2

--- labs/Lab2_Skeleton_Code/planning.cpp
+++ labs/Lab2_Skeleton_Code/planning.cpp
@@ -1,4 +1,5 @@
 #include "planning.h"
+
 /**********************************************************************************************************
   Robot PLANNING - using the sensing to make decisions
  **********************************************************************************************************/
@@ -17,26 +18,26 @@ void RobotPlanning(void) {
 void fsmCollisionDetection() {
   static int collisionDetectionState = 0;
   //Serial.println(collisionDetectionState); //uncomment for debugging
-  
+
   switch (collisionDetectionState) {
     case 0: //collision detected
       //There is an obstacle, stop the robot
       ActionCollision = COLLISION_ON; // Sets the action to turn on the collision LED
-      /* Add code in milestone 2 to stop the robot's wheels - Hint: ActionRobotDrive = ________ */
-      
-      
+      ActionRobotDrive = DRIVE_STOP;
+
+
       //State transition logic
       if ( SensedCollision == DETECTION_NO) {
         collisionDetectionState = 1; //if no collision, go to no collision state
       }
       break;
-    
+
     case 1: //no collision
       //There is no obstacle, drive the robot
       ActionCollision = COLLISION_OFF; // Sets action to turn off the collision LED
 
       fsmSteerRobot(); // Milestone 2
-      
+
       //State transition logic
       if ( SensedCollision == DETECTION_YES ) {
         collisionDetectionState = 0; //if collision, go to collision state
@@ -47,7 +48,25 @@ void fsmCollisionDetection() {
       {
         collisionDetectionState = 0;
       }
-      break;
+  }
+}
+
+////////////////////////////////////////////////////////////////////
+// Code for determining the state of steerRobotState
+////////////////////////////////////////////////////////////////////
+
+int getSteerRobotState() {
+  if ((SensedLightLeft == 0) && (SensedLightRight == 0)) {
+    return 0; //Stop
+  }
+  else if ((SensedLightLeft == 1) && (SensedLightRight == 0)) {
+    return 1; //turn left
+  }
+  else if ((SensedLightLeft == 0) && (SensedLightRight == 1)) {
+    return 2; //turn right
+  }
+  else {
+    return 3; //go straight
   }
 }
 
@@ -55,59 +74,56 @@ void fsmCollisionDetection() {
 // State machine for detecting if light is to the right or left,
 // and steering the robot accordingly.
 ////////////////////////////////////////////////////////////////////
+
 void fsmSteerRobot() {
   static int steerRobotState = 0;
-  //Serial.println(steerRobotState); //uncomment for debugging
+  //  Serial.println(steerRobotState); //uncomment for debugging
 
-  /* Get rid of this whole line for milestone 2
   switch (steerRobotState) {
     case 0: //light is not detected
       //The light is straight ahead, drive straight
       ActionRobotDrive = DRIVE_STOP;
-      
-      //State transition logic
-      if ( SensedLightLeft == DETECTION_YES ) {
-        steerRobotState = 1; //if light on left of robot, go to left state
-      } else if ( SensedLightRight == DETECTION_YES ) {
-        steerRobotState = 2; //if light on right of robot, go to right state
-      }
+      steerRobotState = getSteerRobotState();
       break;
-    
+
     case 1: //light is to the left of robot
       //The light is on the left, turn left
-      ActionRobotDrive =  //Add appropriate variable to set the action to turn left
-      
-      //State transition logic
-      if ( *Add code: If light is also to the right, the light is in front* ) {
-        *Add code to transition to the "light on left and right" state* //if light is on right, then go straight
-      } else if ( *Add code: no longer light to the left ) {
-        *Add transition code* //if light is not on left, go back to stop state
-      }
-      
+      ActionRobotDrive =  DRIVE_LEFT;
+      steerRobotState = getSteerRobotState();
       break;
-    
+
     case 2: //light is to the right of robot
       //The light is on the right, turn right
-      *Add code to set the action*
-      
-      //State transition logic
-      *Add code to transition to the "light on right and left" state 
-
+      ActionRobotDrive =  DRIVE_RIGHT;
+      steerRobotState = getSteerRobotState();
       break;
-      
-    // light is on both right and left
-      *Add Code: Add in a case 3 for when the light is on both the right and left 
-      *Think about what actions you need to implement and 
-      *what changes could occur that would cause a transition to another 
-      *state. Don't forget the break statement at the end of the case.
-      
-      
+
+    case 3:
+      ActionRobotDrive = DRIVE_STRAIGHT;
+      steerRobotState = getSteerRobotState();
+      //control the LEDs
+      break;
+
     default: // error handling
-    {
-      steerRobotState = 0;
-    }
+      {
+        steerRobotState = 0;
+      }
   }
-  Get rid of this whole line for milestone 2*/
+}
+
+
+////////////////////////////////////////////////////////////////////
+// Helper funciton for fsmMoveServoUpAndDown
+// Checks the state of the sensors and changes moveServoState accordingly
+////////////////////////////////////////////////////////////////////
+int getServoState() {
+  if (((SensedLightDown == 1) && (SensedLightUp == 0))) {
+    return SERVO_MOVE_DOWN;
+  } else if (((SensedLightDown == 0) && (SensedLightUp == 1))) {
+    return SERVO_MOVE_UP;
+  } else {
+    return SERVO_MOVE_STOP;
+  }
 }
 
 ////////////////////////////////////////////////////////////////////
@@ -117,14 +133,37 @@ void fsmSteerRobot() {
 void fsmMoveServoUpAndDown() {
   static int moveServoState = 0;
   //Serial.println(moveServoState); //uncomment for debugging
-  
+
   // Milestone 3
   //Create a state machine modeled after the ones in milestones 1 and 2
   // to plan the servo action based off of the perception of the robot
   //Remember no light or light in front = servo doesn't move
   //Light above = servo moves up
   //Light below = servo moves down
-  
+
+  switch (moveServoState) {
+    case SERVO_MOVE_STOP: //Light is detected evenly across the sensors
+      moveServoState = getServoState();
+      ActionServoMove = SERVO_MOVE_STOP;
+      break;
+
+    case SERVO_MOVE_UP: //Light is detected by the upper sensor
+      moveServoState = getServoState();
+      ActionServoMove = SERVO_MOVE_UP;
+      break;
+
+    case SERVO_MOVE_DOWN: //Light is detected by the lower sensor
+      moveServoState = getServoState();
+      ActionServoMove = SERVO_MOVE_DOWN;
+      break;
+
+    default:  //bug handling
+      {
+        moveServoState = SERVO_MOVE_STOP;
+      }
+
+  }
+
 }
 
 ////////////////////////////////////////////////////////////////////
